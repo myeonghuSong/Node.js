38f130876b5fc47a71bb9d4cced858c149927f24
myeonghuSong <21300390@handong.edu>
2019-09-27 18:18:02 +0900

    template 세팅

---
+++ node-cli/template.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const type = process.argv[2];
+const name = process.argv[3];
+const directory = process.argv[4] || '.';
+
+const htmlTemplate = `<DOCTYPE html>
+<html>
+<head>
+    <meta chart="utf-8" />
+    <title>Template</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>CLI</p>
+</body>
+</html>`;
+
+const routerTemplate = `const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    try {
+        res.send('ok');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+module.exports = router;`;
+
