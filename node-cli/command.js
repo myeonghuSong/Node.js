3306fec433f99b289ffaa260f79a175d23e0979d
myeonghuSong <21300390@handong.edu>
2019-09-27 20:56:12 +0900

    commander 프레임워크를 활용한 template 생성

--- node-cli/command.js
+++ node-cli/command.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
+const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-
-let rl;
-let type = process.argv[2];
-let name = process.argv[3];
-let directory = process.argv[4] || '.';
 
 const htmlTemplate = `<DOCTYPE html>
 <html>
@@ -76,44 +72,25 @@ const makeTemplate = (type, name, directory) => {
     }
 };
 
-const dirAnswer = (answer) => {
-    directory = (answer && answer.trim() || '.');
-    rl.close();
-    makeTemplate();
-};
+program
+    .version('0.0.1', '-v, --version')
+    .usage('[options]')
+    .command('template <type>')
+    .usage('--name <name> --path [path]')
+    .description('템플릿을 생성합니다.')
+    .alias('tmpl')
+    .option('-n, --name <name>', '파일명을 입력하세요.', 'index')
+    .option('-d, --directory [path]', '생성 경로를 입력하세요.', '.')
+    .action((type, options) => {
+        makeTemplate(type, options.name, options.directory)
+    })
 
-const nameAnswer = (answer) => {
-    if(!answer || !answer.trim()) {
-        console.clear();
-        console.log('name을 반드시 입력하셔야 합니다.');
-        return rl.question('파일명을 설정하세요. ', nameAnswer);
-    }
-    name = answer;
-    return rl.question('저장할 경로를 설정하세요. (설정하지 않으면 현재경로) ', dirAnswer);
-};
+program
+    .command('*', { noHelp: true })
+    .action(() => {
+        console.log('해당 명령어를 찾을 수 없습니다.')
+        program.help();
+    })
 
-const typeAnswer = (answer) => {
-    if(answer !== 'html' && answer !== 'express-router') {
-        console.clear();
-        console.log('html 또는 express-route만 지원합니다.');
-        return rl.question('어떤 tempalte이 필요하십니까? ', typeAnswer);
-    }
-    type = answer;
-    return rl.question('파일명을 설정하세요 ', nameAnswer);
-};
-
-const program = () => {
-    if(!type || !name) {
-        rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-        console.clear();
-        rl.question('어떤 template이 필요하십니까? ', typeAnswer);
-    }
-    else {
-        makeTemplate();
-    }
-};
-
-program();
+program
+    .parse(process.argv)
