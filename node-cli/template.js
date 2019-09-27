ea94de4bbdb29ab9bcb2c9213478d1792bbfca7e
myeonghuSong <21300390@handong.edu>
2019-09-27 18:54:15 +0900

    cli를 통한 template 파일 생성
    cli html main public/html
    cli express-router index ./routes

--- node-cli/template.js
+++ node-cli/template.js
@@ -32,3 +32,55 @@ router.get('/', (req, res, next) => {
 
 module.exports = router;`;
 
+const mkdirp = (dir) => {
+    const dirname = path.relative('.', path.normalize(dir)).split(path.sep).filter(p => !!p);
+    dirname.forEach((d, idx) => {
+        const pathBuilder = dirname.slice(0, idx+1).join(path.sep);
+        if(!exist(pathBuilder)) {
+            fs.mkdirSync(pathBuilder);
+        }
+    });
+};
+
+const exist = (dir) => {
+    try {
+        fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+        return true;
+    } catch (e){
+        return false;
+    }
+}
+
+const makeTemplate = () => {
+    mkdirp(directory);
+    if( type === 'html') {
+        const pathToFile = path.join(directory, `${name}.html`);
+        if(exist(pathToFile)) {
+            console.error('이미 해당 파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, htmlTemplate);
+            console.log(pathToFile, '생성 완료');
+        }
+    } else if (type === 'express-router') {
+        const pathToFile = path.join(directory, `${name}.js`);
+        if(exist(pathToFile)) {
+            console.error('이미 해당 파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, routerTemplate);
+            console.log(pathToFile, '생성 완료');
+        }
+    } else {
+        console.error('html 또는 express-router 둘 중 하나를 입력하세요.');
+    }
+};
+
+const program = () => {
+    if(!type || !name) {
+        console.error('사용방법: cli html|express-router 파일명 [생성 경로]');
+    }
+    else {
+        makeTemplate();
+    }
+};
+
+program();
