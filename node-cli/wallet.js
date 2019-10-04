97449bb452049dc7ad8097b4d38c00522caa744c
madSong <https://github.com/myeonghuSong/Node.js>
2019-10-04 16:10:44 +0900

    wlt setting

---
+++ node-cli/wallet.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const program = require('commander');
+const inquirer = require('inquirer');
+
+const { version } = require('./package');
+
+program
+    .version(version, '-v, --version')
+    .usage('[options]');
+
+//수입
+program
+    .command('revenue <monet> <desc>')
+    .description('수입을 기록합니다.')
+    .action(() => {});
+//지출
+program
+    .command('expense <money> <desc>')
+    .description('지출을 기록합니다.')
+    .action(() => {});
+//잔액
+program
+    .command('balance')
+    .description('잔액을 표시합니다.')
+    .action(() => {});
+
+program
+    .command('*')
+    .action(() => {
+        console.log('알 수 없는 명령어입니다.');
+    })
+program.parse(process.argv);
