fbd5787c44b6db22089e2ae9f585617931f7689d
myeonghuSong <21300390@handong.edu>
2019-10-14 20:54:21 +0900

    login logout 구현

--- nodebird/routes/auth.js
+++ nodebird/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { User } = require('../models');
 
 const router = express.Router();
 
-router.post('/join', async (req, res, next) => {
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
     const { email, nick , password } = req.body;
     try {
         const exUser = await User.find({ where: { email }});
@@ -25,8 +27,32 @@ router.post('/join', async (req, res, next) => {
     }
 });
 
-router.post('/login', (req, res, next) => {
-
+//POST /auth/login
+router.post('/login', isNotLoggedIn, (req, res, next) => { // req.body.email, req.body.password
+    passport.authenticate('local', (authError, user, info) => {
+        if(authError) {
+            console.error(authError);
+            return next(authError);
+        }
+        if(!user) {
+            req.flash('loginError', info.message);
+            return req.redirect('/');
+        }
+        return req.login(user, (loginError) =>{
+            if(loginError) {
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        })
+    })
 });
+
+//GET /auth/logout
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+})
 
 module.exports = router;
