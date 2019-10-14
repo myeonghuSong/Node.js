0e5713de2d449b1bc14356972ebcd10d912e681e
myeonghuSong <21300390@handong.edu>
2019-10-14 17:32:04 +0900

    프로젝트 구조 세팅
    기본 라우터와 pug 파일 세팅

---
+++ nodebird/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const indexRouter = require('./routes/page');
+const userRouter = require('./routes/user');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser('nodebirdSecret'));
+app.use(session({
+    resave:false,
+    saveUninitialized: false,
+    secret: 'nodebirdSecret',
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+}));
+app.use(flash());
+
+app.use('/', indexRouter);
+
+app.use((req,res,next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res) => {
+    res.locals.message = err.mesage;
+    res.locals.error = req.app.get('env') === 'development' ? err: {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), ()=>{
+    console.log(`${app.get('port')}번 포트에서 실행중입니다.`);
+});
