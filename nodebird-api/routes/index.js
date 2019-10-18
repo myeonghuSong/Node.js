829bf178c96ab6b9e9356c1faec42c2cba83bdff
myeonghuSong <21300390@handong.edu>
2019-10-19 03:56:06 +0900

    nodebird-api 프로젝트 세팅

---
+++ nodebird-api/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { User, Domain } = require('../models');
+router.get('/', (req, res, next) => {
+    User.findOne({
+        where: { id: req.user && req.user.id || null},
+        include: { model: Domain },
+    })
+        .then((user) => {
+            res.render('login', {
+                user,
+                loginError: req.flash('loginError'),
+                domains: user && user.domains,
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        })
+})
+
+module.exports = router;
