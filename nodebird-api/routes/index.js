5db29fe9b040faab9d25dfb073bb43e9b031993c
myeonghuSong <21300390@handong.edu>
2019-10-19 19:57:20 +0900

    uuid로 clientSecret 생성

--- nodebird-api/routes/index.js
+++ nodebird-api/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uuidv4 = require('uuid/v4');
 const { User, Domain } = require('../models');
 router.get('/', (req, res, next) => {
     User.findOne({
@@ -12,6 +13,22 @@ router.get('/', (req, res, next) => {
                 loginError: req.flash('loginError'),
                 domains: user && user.domains,
             })
+        })
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        })
+});
+
+router.post('/domain', (req, res, next) => {
+    Domain.create({
+        userId: req.user.id,
+        host: req.body.host,
+        type: req.body.type,
+        clientSecret: uuidv4(),
+    })
+        .then(() => {
+            res.redirect('/');
         })
         .catch((error) => {
             console.error(error);
