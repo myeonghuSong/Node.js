728073d52d727c57cc659167687b6b3df88b567d
myeonghuSong <21300390@handong.edu>
2019-10-19 21:28:50 +0900

    jwt token 생성 구현

--- nodebird-api/app.js
+++ nodebird-api/app.js
@@ -11,6 +11,7 @@ const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes');
+const v1 = require('./routes/v1');
 
 const app = express();
 sequelize.sync();
@@ -40,6 +41,7 @@ app.use(passport.session());
 
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
+app.use('v1', v1);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
