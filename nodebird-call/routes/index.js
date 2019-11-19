3fc1b86156d3ef36c1442f9e547fc5f2d76a9400
myeonghuSong <21300390@handong.edu>
2019-11-20 03:58:50 +0900

    cors 커스터마이징

--- nodebird-call/routes/index.js
+++ nodebird-call/routes/index.js
@@ -82,4 +82,9 @@ router.get('/follow', async (req, res, next) => {
         }
     }
 })
+
+router.get('/', (req, res) => {
+    res.render('main', { key: process.env.CLIENT_SECRET });
+});
+
 module.exports = router;
