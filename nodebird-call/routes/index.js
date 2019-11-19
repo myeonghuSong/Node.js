0a163748803d18218c218f2b5126a2e14fc85706
myeonghuSong <21300390@handong.edu>
2019-11-19 19:00:33 +0900

    api 작성 및 호출하기

--- nodebird-call/routes/index.js
+++ nodebird-call/routes/index.js
@@ -26,6 +26,48 @@ router.get('/test', async (req, res, next) => {
         }
         return next(error);
     }
-})
+});
+
+const request = async (req, api) => {
+    try {
+        if(!req.session.jwt) {
+            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            req.session.jwt = tokenResult.data.token;
+        }
+        return await axios.get(`http://localhost:8002/v1/${api}`, {
+            headers: { authorization: req.session.jwt },
+        });
+    } catch (error) {
+        console.error(error);
+        if (error.response.status < 500) {
+            return error.response;
+        }
+        throw error;
+    }
+};
+
+router.get('/mypost', async (req, res, next) => {
+    try{
+        const result = await request(req, 'posts/my');
+        res.json(result.data);
+    } catch(error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+    try {
+        const result = await request(
+            req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
